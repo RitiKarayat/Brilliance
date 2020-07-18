1855f8577e9dabbf55b2897dc8165947c8a26990
Ritik Karayat <ritikkarayat1@gmail.com>
2020-07-18 20:16:33 +0530

    Final
    First Try..!!

---
+++ script.js
@@ -0,0 +1,18 @@
+$('#email').focus(function(){
+  $(this).attr('placeholder','');
+});
+$('#email').focusout(function(){
+  $(this).attr('placeholder','xyz@email.com');
+});
+$('#email2').focus(function(){
+  $(this).attr('placeholder','');
+});
+$('#email2').focusout(function(){
+  $(this).attr('placeholder','xyz@email.com');
+});
+
+
+
+
+
+
